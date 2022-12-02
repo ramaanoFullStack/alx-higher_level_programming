bd705c0bab17a72a5d4c7f72c0ab4a81fa1cded8
ramaanoFullStack <rami.stalin.m@outlook.com>
2022-12-01 23:41:07 -0800

    Modified lists.h with new protoypes

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -10,7 +10,7 @@
 * @next: points to the next node
 *
 * Description: singly linked list node structure
-* for Holberton project
+* for ALX project
 */
 typedef struct listint_s
 {
@@ -22,5 +22,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
 
 #endif
