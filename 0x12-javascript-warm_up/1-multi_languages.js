26503fe9c1d1d657c393d58737b7f77575a5ba97
ramaanoFullStack <rsajcm.m@icloud.com>
2023-03-13 09:53:50 -0700

    a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cVar = 'C is fun';
+const pythonVar = 'Python is cool';
+const javascriptVar = 'JavaScript is amazing';
+console.log(cVar);
+console.log(pythonVar);
+console.log(javascriptVar);
