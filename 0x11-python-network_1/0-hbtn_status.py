088cf245199bb06642bd4d52ed1eb2ce42408739
ramaanoFullStack <rami.stalin.m@outlook.com>
2023-03-31 15:14:51 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status :update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@
 
 import urllib.request
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
         response = request.read()
         print("Body response:")
